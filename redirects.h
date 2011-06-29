0ee75afef23bab9d64b65e004b00ae56a725f6c5
codestation <cesarg9@gmail.com>
2011-06-29 00:10:46 -0430

    Add missing header

--- redirects.h
+++ redirects.h
@@ -1,9 +1,23 @@
 /*
- * redirects.h
- *
- *  Created on: 28/06/2011
- *      Author: code
- */
+    Game Categories Lite 1.0
+    Copyright (C) 2011, codestation
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Stub to a temporary redirect of the hooked funcs
+    (to avoid conflicts with other plguins).
+*/
 
 #ifndef REDIRECTS_H_
 #define REDIRECTS_H_
