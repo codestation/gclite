e8ef8f95f1f6bdb97c74b4a1ac8017969af113c8
codestation <cesarg9@gmail.com>
2011-09-13 21:22:38 -0430

    Add missing header

---
+++ filter.h
@@ -0,0 +1,27 @@
+/*
+ *  this file is part of Game Categories Lite
+ *
+ *  Copyright (C) 2011  Codestation
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FILTER_H_
+#define FILTER_H_
+
+int load_filter();
+void unload_filter();
+int check_filter(const char *str);
+
+#endif /* FILTER_H_ */
